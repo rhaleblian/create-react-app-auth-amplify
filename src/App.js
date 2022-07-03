d744fa6aaade9b47cab97bc28b91a1a60dea3780
Ray Haleblian <ray@haleblian.com>
2022-07-03 09:29:53 -0400

    Prose

--- src/App.js
+++ src/App.js
@@ -53,17 +53,6 @@ class App extends Component {
         <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
 
           <p>
             <button onClick={decode}>
